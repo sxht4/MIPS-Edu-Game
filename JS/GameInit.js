102a049f6f751046be29a29d8c4d22b733c28e35
SaiCao <saicao@buffalo.edu>
2019-02-09 19:13:01 -0500

    mainUI

---
+++ JS/GameInit.js
@@ -0,0 +1,11 @@
+
+
+function initGame() {
+    var gameCanvas = document.getElementById("gameCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 50, 40);
+
+
+}
+initGame();

aa2ee7c0bd8f4550be42ab2b73d94cda8fba2ca8
saicao <1665673333@qq.com>
2019-02-09 20:42:48 -0500

    add canvas canvas size set to 480*320

--- JS/GameInit.js
+++ JS/GameInit.js
@@ -1,11 +1,15 @@
 
-
+initGame();
 function initGame() {
     var gameCanvas = document.getElementById("gameCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, 50, 40);
-
+    var background = gameCanvas.getContext("2d");
+    background.fillStyle = "#f2f2f2";
+    background.fillRect(0, 0,gameCanvas.width,gameCanvas.height );
+    background.font = "20pt sans-serif";
+    background.fillStyle = "blue";
+    background.textAlign = 'center'; 
+    background.fillText("background animations", gameCanvas.width/2, gameCanvas.height/2); 
+    
 
 }
-initGame();
+

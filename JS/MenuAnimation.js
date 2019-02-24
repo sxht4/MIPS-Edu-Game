69efe2baaf1d651bf0f65c16a09afe24d91b0cbe
LordCTY <tianyuca@buffalo.edu>
2019-02-25 04:17:03 +0800

    fix some parameters

--- JS/MenuAnimation.js
+++ JS/MenuAnimation.js
@@ -2,12 +2,13 @@ class MenuAnimation extends Component{
     constructor(){
         var str="Mips Education Game";
         
-        super("MenuAnimation",str,10,50,null,40,false);
+        super("MenuAnimation",str,50,25,0,10,false);
 
         this.speedX = 0;
         this.speedY = 0;    
         this.gravity = 0.1;
         this.gravitySpeed = 0;
+        this.bounce = 0.6;
     }
 
     hitBottom () {
@@ -30,7 +31,7 @@ class MenuAnimation extends Component{
 
         this.move();
         CTX.font = "40px Arial";
-        CTX.clearRect(this.x, this.y,CTX.measureText(this.content),this.height);
+       // CTX.clearRect(this.x, this.y,CTX.measureText(this.content),this.height);
         CTX.strokeText(this.content,this.x,this.y);
 
     }

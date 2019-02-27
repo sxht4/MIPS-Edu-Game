ee1a34ac21c4977a7216b89de473837e47d440d1
Dhlx <375529588@qq.com>
2019-02-27 17:17:56 -0500

    memory

--- JS/Memory.js
+++ JS/Memory.js
@@ -1,13 +1,24 @@
 class Memory{
-    constructor(memory_store){
-        this.memory_store=memory_store; //memory_store is a array that stores value
+    constructor(){
+        this.memory_store=[]; //memory_store is a array that stores value
+        this.maxSize=100;
     }
 
      get(index) {
+         if(index >= maxSize){
+             return null;
+         }
         return memory_store[index];
     }
 
     set(index, value){
+        if(index >= maxSize){
+            return null;
+        }
         memory_store[index]=value;
+    }
+
+    size(){
+        return memory_store.length;
     }
 }

621fbfab9941db37951ccac17acae5a6d78cd859
SaiCao <caosai2016@Gmail.com>
2019-04-09 16:03:50 -0400

    Add HintEvent to alert a hint in game Add hint for IM button Add hint for Add button Document this class

---
+++ JS/Event/HintEvent.js
@@ -0,0 +1,28 @@
+/**
+ * alert a hint when event happen
+ *  
+ * @class HintEvent
+ * @extends {GameEvent}
+ */
+class HintEvent extends GameEvent{
+
+    constructor(id){
+        super(id,0);
+        this.content=null;
+    }
+    setHintContent(content){
+        this.content=content;
+    }
+    /**
+     *excute this event
+     *
+     * @param {number} x
+     * @param {number} y
+     * @memberof HintEvent
+     */
+    excute(x,y){
+        alert(this.content);
+
+    }
+
+}

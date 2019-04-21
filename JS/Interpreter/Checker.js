ad2d717c944b3b65e28837174a575d9fc822ef4f
saicao <1665673333@qq.com>
2019-04-21 12:16:01 -0400

    Implement a tutorial chapater Add removeSence function for Game.js Add checker into interpreter to check result. Fix bug that the getElement function run two time when clicked. Add Dialogue.js to show dialogue instead of  alert Add TransparentComponent to show a Transparent rectangle in tutorial. Add setLocation function for CPU class

---
+++ JS/Interpreter/Checker.js
@@ -0,0 +1,12 @@
+class Checker {
+    constructor(code_panel,r,m){
+        this.code_panel=code_panel;
+        this.r=r;
+        this.m=m;
+
+    }
+    check(){
+
+        throw 'error this should not be called'
+    }
+}

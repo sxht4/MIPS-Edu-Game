9df94bcbeeb7c65c7849e81867e8071124f391ff
LordCTY <tianyuca@buffalo.edu>
2019-04-24 15:17:57 -0400

    Design chapter one and its question

--- JS/Interpreter/Interpreter.js
+++ JS/Interpreter/Interpreter.js
@@ -18,7 +18,6 @@ class Interpreter {
         this.memeory = memeory;
         this.register = register;
         this.test=null;
-     
         this.init();
 
     }
@@ -69,10 +68,6 @@ class Interpreter {
             throw error;
         }
 
-
-
-
-
     }
     /**
      *
@@ -92,9 +87,83 @@ class Interpreter {
         destcell.cell.setContent(R1.value + R2.value);
 
 
+    }
 
+    /**
+     *
+     * Shift Left Logic Instruction
+     * @param {String} dest
+     * @param {String} arg1
+     * @param {String} arg2
+     * @memberof Interpreter
+     */
+    async executeSLL(dest, arg1, arg2) {
+        try {
+            var value2 = Number.parseInt (arg2)
+        } catch (error) {
+            throw 'not a integer'+arg2;
+        }
+        try {
+            var R1 = this.getRegister(arg1);
+            var destCell = this.getRegister(dest);
+            await  this.cpu.moveTo(R1.cell.x, R1.cell.y);
+            await  this.cpu.moveTo(destCell.cell.x, destCell.cell.y);            
+            destCell.cell.setContent(R1.value * Math.pow(2, value2));
+            
+        } catch (error) {
+            throw error;
+        }       
+    }
 
+    /**
+     *
+     * Shift Right Logic Instruction
+     * @param {String} dest
+     * @param {String} arg1
+     * @param {String} arg2
+     * @memberof Interpreter
+     */
+    async executeSRL(dest, arg1, arg2) {
+        try {
+            var value2 = Number.parseInt (arg2)
+        } catch (error) {
+            throw 'not a integer'+arg2;
+        }
+        try {
+            var R1 = this.getRegister(arg1);
+            var destCell = this.getRegister(dest);
+            await  this.cpu.moveTo(R1.cell.x, R1.cell.y);
+            await  this.cpu.moveTo(destCell.cell.x, destCell.cell.y); 
+            for(var i = 0; i < value2; i++){
+                R1.value % 2 == 0? R1.value = R1.value : R1.value = R1.value-1;
+                R1.value = R1.value / 2;    
+            }
+            //R1.value % 2 == 0? R1.value = R1.value : R1.value = R1.value-1;
+            destCell.cell.setContent(R1.value);
+            
+        } catch (error) {
+            throw error;
+        }
+       
+    }
 
+    /**
+     *
+     * Set Less Than Instruction
+     * @param {String} dest
+     * @param {String} arg1
+     * @param {String} arg2
+     * @memberof Interpreter
+     */
+    async executeSLT(dest, arg1, arg2) {
+        var R1 = this.getRegister(arg1);
+        var R2 = this.getRegister(arg2);
+        var destcell = this.getRegister(dest);
+        await  this.cpu.moveTo(R1.cell.x, R1.cell.y);
+        await  this.cpu.moveTo(R2.cell.x, R2.cell.y);
+        await  this.cpu.moveTo(destcell.cell.x, destcell.cell.y);
+        destcell.cell.setContent(R1.value < R2.value? 1: 0);
+       
     }
 
 
@@ -127,6 +196,15 @@ class Interpreter {
                     case "add":
                         await this.executeADD(splited[1], splited[2], splited[3]);
                         break;
+                    case "sll":
+                        await this.executeSLL(splited[1], splited[2], splited[3]);
+                        break;
+                    case "srl":
+                        await this.executeSRL(splited[1], splited[2], splited[3]);
+                        break;
+                    case "slt":
+                        await this.executeSLT(splited[1], splited[2], splited[3]);
+                        break;           
                     case "":
                         break;
                     default:
--- JS/MainScene.js
+++ JS/MainScene.js
@@ -1,53 +1,203 @@
 /**
- * player use this sence to play game
+ * Chapter 1
  * @class MainScene
  * @extends {GameScene}
- * @author Sai Cao,Xiang Li,Tianyu Cao
+ * @author Sai Cao,Tianyu Cao, Xiang Li
  */
 class MainScene extends GameScene {
-    /**
-     *Creates an instance of MainScene.
-     * @memberof MainScene
-     */
     constructor() {
         super();
+        this.except_component = null;
+        this.ErrorText = null;
+        this.modcount = 0;
+        this.count = 0;
+
+
+
+
     }
-    
-/**
- *
- * init this scene
- * @memberof MainScene
- */
-init() {
+    init() {
+        this.layers = [];
+        this.layers.push(new Layer());
+        this.initScene();
+        this.problem();
+
+
+
+
+    }
+    initScene() {
         console.log("init GaneUI Menu");
-        var cp = new  CodePanel();
-        this.addComponent(cp, -1);
+        this.cp = new CodePanel();
+        this.addComponent(this.cp, -1);
 
+        this.GoalButton = Button.getButton('C1Goal', 375, 0, 1, 3, 'Goal');
+        var C1GoalEvent=new HintEvent('C1Goal');
+        C1GoalEvent.setHintContent("Compute function t0 = 31*(t1^2) + 4*t2 + 7");
+        this.GoalButton.addClickEvent(C1GoalEvent);
+        this.addComponent(this.GoalButton,-1);
         //Instruction Menu Button 
-        var InstructionMenuButton = Button.getButton('Instruction_Menu', 375, 30, 1, 3, 'IM');
-        InstructionMenuButton.addClickEvent(new NewSceneEvent(new InstructionMenu()));
-        var IMHint= new HintEvent('IMHint')
+        this.InstructionMenuButton = Button.getButton('Instruction_Menu', 375, 30, 1, 3, 'IM');
+        this.InstructionMenuButton.addClickEvent(new NewSceneEvent(new InstructionMenu()));
+        var IMHint = new HintEvent('IMHint')
         IMHint.setHintContent('this is instruction menu \n it contain all insturtions game support');
-        InstructionMenuButton.addLongPressEvent(IMHint);
-        this.addComponent(InstructionMenuButton, -1);
-
+        this.InstructionMenuButton.addLongPressEvent(IMHint);
+        this.addComponent(this.InstructionMenuButton, -1);
         //run button
-        var RunButon = Button.getButton('Code_Run', 375, 60, 1, 3, 'Run');
-        this.addComponent(RunButon, -1);
-        var m = new Memory("Memory", [], 5, 5, 50, 200, false);
-        m.add(0);
-        this.addComponent(m, -1);
-
-        var r = new Register("Register", [], 100, 270, 250, 50, true);
-        r.init();
-        this.addComponent(r, -1);
+        this.RunButon = Button.getButton('Code_Run', 375, 60, 1, 3, 'Run');
+        this.addComponent(this.RunButon, -1);
+        this.m = new Memory("Memory", [], 5, 5, 50, 200, false);
+        this.m.add(0);
+        this.addComponent(this.m, -1);
+        this.r = new Register("Register", [], 100, 270, 250, 50, true);
+        this.r.init();
+        this.addComponent(this.r, -1);
+        this.addLayer();
+        this.cpu = new CPU();
+        this.addComponent(this.cpu, -1);
+        this.cpu.setSpeed(2);
+        this.interpreter=new Interpreter(this.cpu, this.cp, this.m, this.r);
+        this.RunButon.addClickEvent(new RunCodeEvent( this.interpreter));
+    }
+    problem() {
+        this.cp.instructions = [''];
+        
 
         this.addLayer();
-        var cpu = new CPU();
-        this.addComponent(cpu, -1);
-        cpu.setSpeed(3);
+        //this.highLight(this.cp);
+        var pro = new Dialogue('problem 1');
+        pro.width = 250;
+        pro.appendLine('Chapter One: Basic Arithmetic');
+        pro.appendLine('In this chapter, you are required to');
+        pro.appendLine('computer the following function:');
+        pro.appendLine('t0 = 31*(t1^2) + 4*t2 + 7');
+        pro.appendLine('click green area to continue');
+        this.except_component = pro;
+        //this.ErrorText = 'you should click second line';
+        this.addComponent(pro, -1);
 
-        RunButon.addClickEvent(new RunCodeEvent(new Interpreter(cpu, cp, m, r)));
-      
+
     }
+
+    highLightByID(str) {
+
+        var component = this.getByID(str);
+        if (component == null) {
+            throw 'fail to high light ' + str
+        }
+
+
+
+    }
+    highLight(component) {
+        this.inverseSelect(component);
+
+
+    }
+
+    /**
+     *
+     *
+     * @param {Component} component
+     * @memberof Tutorial
+     */
+    inverseSelect(component, color) {
+
+        this.addComponent(new TransparentComponent('Left', 0, 0, component.x, GAME.height), -1);
+        this.addComponent(new TransparentComponent('Riht', component.x + component.width, 0, GAME.width - (component.x + component.width), GAME.height), -1);
+        this.addComponent(new TransparentComponent('Top', component.x, 0, component.width, component.y), -1);
+        this.addComponent(new TransparentComponent('Down', component.x, component.y + component.height, component.width, GAME.height - (component.y + component.height)), -1);
+
+
+    }
+
+
+
+    getClickedElement(x, y) {
+        console.log('get click');
+        var component = this.private_getClickElement(x, y);
+
+        if (component != null) {
+            console.log(component);
+            if (this.except_component != null) {
+                if (this.isClicked(x, y, this.except_component)) {
+
+                    this.count++;
+                    console.log(this.count);
+                    this.except_component = null;
+                    console.log(component);
+                    return component;
+                } else {
+                    alert(this.ErrorText);
+                    return null;
+                }
+
+            }
+            return component;
+
+        }
+        return null;
+
+
+
+    }
+    private_getClickElement(x, y) {
+        for (var i = this.layers.length - 1; i >= 0; i--) {
+            var component = this.layers[i].getClickedElement(x, y);
+            if (component != null) {
+                return component;
+
+            }
+        }
+        return null;
+    }
+
+    nextStep() {
+
+
+        switch (this.count) {
+
+            case 1:
+                this.backToScene();
+
+                break;
+            case 2:
+        
+        break;
+
+        }
+    }    
+
+backToScene() {
+    this.removeLayer(-1);
+    this.addLayer();
+    this.r.getCellAt(1).setContent(4);
+    this.r.getCellAt(2).setContent(13);
+    this.interpreter.test=new Chapter1Test(this.cp,this.r,this.m);
+
+
+    this.count++;
+
+
+}
+
+
+
+updateFrame() {
+    this.nextStep();
+    for (var i = 0; i < this.layers.length; i++) {
+        this.layers[i].updateLayer();
+    }
+
+}
+
+isClicked(x, y, component) {
+    if (x >= component.x && x <= component.x + component.width) {
+        if (y >= component.y && y <= component.y + component.height) {
+            return true;
+        }
+    }
+    return false;
+}
+
 }

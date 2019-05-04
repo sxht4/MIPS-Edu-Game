bad40dedf7d6c32eb726b8b7f094439c1c20fb57
SaiCao <caosai2016@Gmail.com>
2019-05-04 15:49:07 -0400

    Add UnitTest for change page feature Content test Page index test

--- testScripts/test.js
+++ testScripts/test.js
@@ -848,3 +848,37 @@ describe('RegisterCell.js Unit Tests', function(){
         assert.equal(RegCell.clickable, true);
     });
 });
+
+
+describe('CodePanel.js Change Page Unit Tests', function(){
+    var cp = new CodePanel();
+    for(var i=0;i<30;i++){
+        cp.instructions.push('ins'+i);
+    }
+    it('changePage (1) should change the page to next page', function(){
+       cp.changePage(1);
+       assert.equal(cp.line_index,13 );
+    }); 
+    it('Constructor should set content correctly', function(){
+        cp.changePage(1);
+        assert.equal(cp.line_index,13 );
+        assert.equal(cp.getContent(1).content,'ins 14');
+
+    }); 
+
+    it('changePage (-1) should change the page to content', function(){
+    
+        cp.changePage(1);
+        cp.changePage(-1);
+        assert.equal(cp.line_index,0 );
+     }); 
+     it('changePage (-1) should change the content', function(){
+         cp.changePage(1);
+         cp.changePage(1);
+         cp.changePage(-1);
+         assert.equal(cp.line_index,13 );
+         assert.equal(cp.getContent(1).content,'ins 14');
+     }); 
+    
+});
+
